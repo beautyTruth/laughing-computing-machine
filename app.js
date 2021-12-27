f379e8fb5b6ec7c53aeda43b1168103ad6b65fda
Phil <happy.melodist@gmail.com>
2021-12-26 22:25:14 -0600

    write async await functionality for retreiving a joke from icanhazdadjoke api

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-// https://icanhazdadjoke.com/api
+// // https://icanhazdadjoke.com/api
+
+// const jokeBtn = document.querySelector("button");
+// const jokePara = document.querySelector(".joke");
+// console.log(jokePara);
+
+// jokeBtn.addEventListener("click", getJoke);
+
+// async function getJoke() {
+//   const jokeURL = "https://icanhazdadjoke.com/";
+//   const response = await fetch(jokeURL, {
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   });
+
+//   const responseData = await response.json();
+//   jokePara.innerText = responseData.joke;
+// }
+
+// be here now
+
+/*
+my code below
+*/
 
 const jokeBtn = document.querySelector("button");
 const jokePara = document.querySelector(".joke");
@@ -14,6 +38,6 @@ async function getJoke() {
     },
   });
 
-  const responseData = await response.json();
-  jokePara.innerText = responseData.joke;
+  const tiddy = await response.json();
+  jokePara.innerText = tiddy.joke;
 }
